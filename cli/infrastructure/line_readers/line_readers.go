a2533095569f483798e31d0071ac54de8bcc23b8
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2025-06-05 14:43:41 +0200

    chore: suppress useless print

--- cli/infrastructure/line_readers/line_readers.go
+++ cli/infrastructure/line_readers/line_readers.go
@@ -15,7 +15,6 @@ func (clrdr *CliReader) Run() (string, interfaces.IResponse) {
 	fmt.Print("Enter a line: ")
 	reader := bufio.NewReader(os.Stdin)
 	line, error := reader.ReadString('\n')
-	print("\n")
 	if error != nil {
 		resp_builder := domain.NewResponseBuilder()
 		resp_builder.Error(fmt.Sprintln("Cli reader : ", error.Error()))
